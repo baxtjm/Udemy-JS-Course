854bc18e26f7ad45c64afb02fcc05fc2a593ad2f
baxtjm <baxtjm@gmail.com>
2017-05-27 20:18:13 -0400

    Up to First Coding Challenge

--- script.js
+++ script.js
@@ -112,13 +112,71 @@ if (23 === '23') {
 
 //lecture: boolean logic and switch
 
-var age = 23;
+/*
+var age = 35;
 
 if (age < 20) {
     console.log('John is a teenager');
-} else {
+} else if (age >= 20 && age < 30)  {
+    console.log('John is a young man!');
+}
+else {
     console.log('John is a man');
 }
+
+var job = 'banker';
+
+job = prompt('What does John do?')
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids.');
+        break
+    case 'driver':
+        console.log('John drives a cab in Libson.');
+        break;
+    case 'cop':
+        console.log('John helps fight crime.');
+        break;
+    default: 
+        console.log('John does something else.');
+           }
+*/
+
+
+var johnHeight = 172;
+var johnAge = 26;
+
+var friendHeight = 29;
+var friendAge = 165;
+
+var johnScore = johnHeight + (5 * johnAge);
+var friendScore = friendHeight + (5 * friendAge);
+
+/*
+if (johnScore > friendScore) {
+    console.log("Johns score is higher!");
+} else if (johnScore === friendScore) {
+    console.log("Scores are the same");
+} else {
+    console.log("Friend's score is higher!");
+}
+*/
+
+var maryHeight = 158;
+var maryAge = 31;
+var maryScore = maryHeight + 5 * maryAge;
+
+if (johnScore > friendScore && johnScore > maryScore) {
+    console.log('John wins the game');
+} else if (friendScore > johnScore && friendScore > maryScore) {
+    console.log('Friend wins the game');
+} else if (maryScore > johnScore && maryScore > friendScore) {
+    console.log('Mary wins the game')
+} else {
+    console.log("It's a draw")
+}
+
 
 
 
